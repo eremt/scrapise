4840600edde258554910d23205b54f5333894cf1
User <eremtroy@gmail.com>
2019-11-15 23:18:13 +0100

    WIP: refactor to a more functional aproach

--- index.js
+++ index.js
@@ -6,6 +6,61 @@ const optionsDefault = {
 }
 
 /**
+ * Fetch url body
+ * @param {String} url
+ * @return {Promise}
+ */
+function get (url) {
+  return fetch(url).then(response => response.text())
+}
+
+/**
+ * Parses a schema
+ * @param {Object} schema The schema to parse
+ * @param {Object|String} context Optional context
+ * @return {Object} Object matching the schema
+ */
+function parse (schema, $, context = null) {
+  return Object.entries(schema).reduce((obj, [key, value]) => {
+    let type = typeof value
+    if (Array.isArray(value)) type = 'array'
+    let result = null
+
+    switch (type) {
+      case 'string':
+        const [firstMatch] = getCheerioText(value, $, context)
+        result = firstMatch || null
+        break;
+
+      case 'array':
+        const [ listSchema, listContext ] = value
+
+        if (typeof listSchema === 'string') {
+          result = getCheerioText(listSchema, $, context)
+        } else {
+          const matches = $(listContext, context)
+
+          result = matches.map((_, elem) => {
+            return parse(listSchema, $, elem)
+          }).get()
+        }
+        break;
+
+      case 'function':
+        result = value($, context)
+        break;
+
+      default:
+        result = parse(value, $, context)
+        break;
+    }
+
+    obj[key] = result
+    return obj
+  }, {})
+}
+
+/**
  * Helper function to extract text from all matches
  * @param {String} selector
  * @param {Object} $ Cheerio object
@@ -17,84 +72,28 @@ function getCheerioText (selector, $, context = null) {
   return $(selector, context).map((_, elem) => $(elem).text().trim()).get()
 }
 
+/**
+ * Create a location object from href
+ * @param {String} href
+ * @param {Object} location Optional location object to replace missing pieces of the url
+ * @return {Object} Location object
+ */
+function getLocation (href, location = null) {
+  return {
+    href
+  }
+}
+
 class Scrapise {
   constructor (url, schema, options = optionsDefault) {
     this.options = options
-    this.location = this.getLocation(url)
+    this.location = getLocation(url)
 
-    return this.get(url)
-      .then(body => {
+    return get(url).then(body => {
         const $ = cheerio.load(body, { decodeEntities: true })
 
-        return this.parse(schema, $)
+        return parse(schema, $)
       })
-  }
-
-  /**
-   * Get url body
-   * @param {String} url
-   * @return {Promise}
-   */
-  get (url) {
-    return fetch(url).then(response => response.text())
-  }
-
-  /**
-   * Create a location object from href
-   * @param {String} href
-   * @param {Object} location Optional location object to replace missing pieces of the url
-   * @return {Object} Location object
-   */
-  getLocation (href, location = null) {
-    return {
-      href
-    }
-  }
-
-  /**
-   * Parses a schema
-   * @param {Object} schema The schema to parse
-   * @param {Object|String} context Optional context
-   * @return {Object} Object matching the schema
-   */
-  parse (schema, $, context = null) {
-    return Object.entries(schema).reduce((obj, [key, value]) => {
-      let type = typeof value
-      if (Array.isArray(value)) type = 'array'
-      let result = null
-
-      switch (type) {
-        case 'string':
-          const [firstMatch] = getCheerioText(value, $, context)
-          result = firstMatch || null
-          break;
-
-        case 'array':
-          const [ listSchema, listContext ] = value
-
-          if (typeof listSchema === 'string') {
-            result = getCheerioText(listSchema, $, context)
-          } else {
-            const matches = $(listContext, context)
-
-            result = matches.map((_, elem) => {
-              return this.parse(listSchema, $, elem)
-            }).get()
-          }
-          break;
-
-        case 'function':
-          result = value($, context)
-          break;
-
-        default:
-          result = this.parse(value, $, context)
-          break;
-      }
-
-      obj[key] = result
-      return obj
-    }, {})
   }
 }
 
